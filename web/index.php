a1ed5774d6b7c950ca69666d9a33940ba0c4fd15
Fernando Hernandez <chivista3001@icloud.com>
2020-09-26 23:06:14 -0700

    Update
    This push allows to use GitHub Desktop app to deploy to Github repo

--- web/index.php
+++ web/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=h1, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome to Fernando Hernandez Portal</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
@@ -34,7 +34,7 @@
                     <h1 class="heading-primary moveInLeft">Designig and Developing great Websites and Apps</h1>
                     <p class="paragraph">Hello and welcome! I created this website for my CSE 341 - Web Backend Development II class. Here will find all my assignments in for this class.</p>
                     <p class="paragraph">I plan to continue improving this website and design and in the near future will subtitute my current portfolio website at <a href="https://fhwebdeveloper.com" target="_blank">fhwebdeveloper.com</a></p>
-                    <a href="#" class="action-link u-margin-bottom-medium">View Assignments</a>
+                    <a href="assignments.php" class="action-link u-margin-bottom-medium">View Assignments</a>
                 </div>
                 <div class="col-1-of-2 u-center-text">
                     <img src="img/profile.png" alt="Fernando Hernandez Photo">
