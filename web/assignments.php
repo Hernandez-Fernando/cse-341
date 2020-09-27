bc991bc50558c2c012035f4dc4474cf6251604fc
Fernando Hernandez <chivista3001@icloud.com>
2020-09-26 22:58:52 -0700

    Fixed Bugs

---
+++ web/assignments.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assignments | Fernando Hernandez Portal</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="navigation">
+        <input type="checkbox" class="navigation-checkbox" id="nav-toggle">
+        <label for="nav-toggle" class="navigation-button">Menu<span class="navigation-icon">&nbsp;</span></label>
+        <div class="navigation-background">&nbsp;</div>
+        <nav class="navigation-nav">
+            <ul class="navigation-list">
+                <li class="navigation-item"><a href="index.php" class="navigation-link">Homepage</a></li>
+                <li class="navigation-item"><a href="assignments.php" class="navigation-link">Assignments</a></li>
+                <li class="navigation-item"><a href="#" class="navigation-link">About Me</a></li>
+            </ul>
+        </nav>
+    </div>
+    <header class="header">
+        <div class="row">
+            <h2 class="brand">
+                <span class="brand-main">Fernando Hernandez</span>
+                <span class="brand-sub">Web Designer &amp; Developer</span>
+            </h1>
+        </div>
+    </header>
+    <main class="main">
+        <div class="row">
+            <section class="section-main">
+                <div class="u-center-text">
+                    <h1 class="heading-primary gradient-text">Coming Soon</h1>
+                    <p class="paragraph">Sorry, I'm working on my assignments, so, please come back soon.</p>
+                </div>
+            </section>
+        </div>
+    </main>
+    <footer class="footer u-margin-bottom-big">
+        <div class="row">
+            <div class="col-1-of-2">
+                <div class="footer-navigation">
+                    <ul class="footer-list">
+                        <li class="footer-item"><a href="assignments.php" class="footer-link">Assignments</a></li>
+                        <li class="footer-item"><a href="https://fhwebdeveloper.com/portfolio" class="footer-link" target="_blank">View Portfolio</a></li>
+                        <li class="footer-item"><a href="#" class="footer-link">About Me</a></li>
+                    </ul>
+                </div>
+                <p class="footer-copyright">&copy; <?php echo date("Y"); ?> - All Rights Reserved - Fernando Hernandez</p>
+            </div>
+            <div class="col-1-of-4 social-div">
+                    <a href="https://facebook.com/fhwebdeveloper" class="social-link" target="_blank">
+                        <svg class="social-icon">
+                            <use xlink:href="img/symbol-defs.svg#icon-facebook"></use>
+                        </svg>
+                    </a>
+                    <a href="https://instagram.com/fhwebdeveloper" class="social-link" target="_blank">
+                        <svg class="social-icon">
+                            <use xlink:href="img/symbol-defs.svg#icon-instagram"></use>
+                        </svg>
+                    </a>
+                    <a href="https://twitter.com/fhwebdeveloper" class="social-link" target="_blank">
+                        <svg class="social-icon">
+                            <use xlink:href="img/symbol-defs.svg#icon-twitter"></use>
+                        </svg>
+                    </a>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
