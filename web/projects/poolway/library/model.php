0930d8704cdd2d9a0e3bb18fdefaea0ac9572a8d
Fernando Hernandez <chivista3001@icloud.com>
2020-10-18 08:39:49 -0700

    Update

--- web/projects/poolway/library/model.php
+++ web/projects/poolway/library/model.php
@@ -1,29 +1,6 @@
 <?php
-try
-		{
-		  $dbUrl = getenv('DATABASE_URL');
-		
-		  $dbOpts = parse_url($dbUrl);
-		
-		  $dbHost = $dbOpts["host"];
-		  $dbPort = $dbOpts["port"];
-		  $dbUser = $dbOpts["user"];
-		  $dbPassword = $dbOpts["pass"];
-		  $dbName = ltrim($dbOpts["path"],'/');
-		
-		  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-		
-		  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		}
-		catch (PDOException $ex)
-		{
-		  echo 'Error!: ' . $ex->getMessage();
-		  die();
-	  }
 
-
-
-function getCustomerList() {
+function getCustomerList($db) {
 	//$db = dbConnection();
 	//$sql = 'SELECT customerId, firstName, lastName, address, city, state, zcode, phone FROM customers ORDER BY firstName ASC';
 	$stmt = $db->prepare('SELECT customerId, firstName, lastName, address, city, state, zcode, phone FROM customers ORDER BY firstName ASC');
