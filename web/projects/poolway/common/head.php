b6dfa22294964534add613c4008f4291f89faded
Fernando Hernandez <chivista3001@icloud.com>
2020-10-18 07:52:58 -0700

    Update

--- web/projects/poolway/common/head.php
+++ web/projects/poolway/common/head.php
@@ -1,3 +1,3 @@
-<link rel="stylesheet" href="/css/style.css">
+<link rel="stylesheet" href="/css/main.css">
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
 <script src="https://kit.fontawesome.com/c141cd3159.js" crossorigin="anonymous"></script>
