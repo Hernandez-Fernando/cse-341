efac95faebe0d7a9187f40e0e6b5a1b6d94aef9d
Fernando Hernandez <chivista3001@icloud.com>
2020-10-18 08:26:55 -0700

    Update customer-list.php

--- web/projects/poolway/customer-list.php
+++ web/projects/poolway/customer-list.php
@@ -2,7 +2,7 @@
 	ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-	//require_once 'projects/poolway/library/connections.php';
+	require_once(__DIR__ . '/library/connections.php');
 	require_once(__DIR__ . '/library/model.php');
 	//require_once 'library/functions.php';
 
